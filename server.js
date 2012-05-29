1af1a335633a5a76523edc9555bf4733e688f4eb
Rave Classic Work <raveclassic@gmail.com>
2012-05-29 18:48:12 +0400

    websockets

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
-var http = require("http");
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello,  World");
-  response.end();
-}).listen(8080); 
+app.listen(8080);
+
+var users = new Array();
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500, {"Content-Type": "text/plain"});
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.end(data);
+  });
+}
+ 
+io.sockets.on('connection', function (socket) {
+		console.log("new client!");
+		console.dir(socket);
+		socket.emit('logon', { currentOnline: users.length });
+		socket.on('my other event', function (data) {
+		console.log(data);
+	});
+});
