ad37d4fb102b85946610e72eb7adbb2fce3449d0
raveclassic-home <raveclassic@gmail.com>
2012-05-30 02:48:02 +0400

    df

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function(request, response) {
    response.send("hello world!");
 });
 
-var port = process.env.port || 8080;
+var port = process.env.PORT || 8080;
 
 app.listen(port, function() {
     console.log("Listening on port: "+port);
