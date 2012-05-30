caea5c9c7a02d59cfd72fb7c4455dfbb3f14a2e4
raveclassic-home <raveclassic@gmail.com>
2012-05-30 14:30:58 +0400

    working

--- server.js
+++ server.js
@@ -1,65 +1,52 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-//var io = require('socket.io').listen(app);
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
 var fs = require('fs');
 var db = require('./database.js');
 
 var test = require('./test.js');
 
-//LOGIC
-app.get('/', function(request, response) {
-   response.send("hello world!");
-});
-
-app.get('/favicon.ico', function(request, respose) {
-
-});
-
-var port = process.env.PORT || 4040;
+var port = process.env.PORT || 8080;
 
 app.listen(port, function() {
-    console.log("Listening on port: "+port);
+    console.log("Listening on port: ", port);
 });
+var users = new Array();
+db.open(
+    function() {
+        console.log("Connection established");
+        console.log("Waiting for requests..");
+        io.sockets.on('connection',
+            function(socket) {
+                console.log("new client!");
+                socket.emit('logon', {
+                    currentOnline: users.length
+                });
+                socket.on('my other event',
+                    function(data) {
+                        console.log(data);
+                    });
+            }
+        );
+    },
+    function(err) {
+        console.dir(err);
+    }
+);
 
-//app.listen(8080);
-//    var users = new Array();
-//    db.open(
-//        function() {
-//            console.log("Connection established");
-//            console.log("Waiting for requests..");
-//            io.sockets.on('connection',
-//                function(socket) {
-//                    console.log("new client!");
-//                    socket.emit('logon', {
-//                        currentOnline: users.length
-//                    });
-//                    socket.on('my other event',
-//                        function(data) {
-//                            console.log(data);
-//                        });
-//                }
-//            );
-//        },
-//        function(err) {
-//            console.dir(err);
-//        }
-//    );
-//
-//
-//function handler(req, res) {
-//    fs.readFile(__dirname + '/index.html',
-//        function(err, data) {
-//            if (err) {
-//                res.writeHead(500, {
-//                    "Content-Type": "text/plain"
-//                });
-//                return res.end('Error loading index.html');
-//            }
-//
-//            res.writeHead(200, {
-//                "Content-Type": "text/html"
-//            });
-//            res.end(data);
-//        });
-//}
+
+function handler(req, res) {
+    fs.readFile(__dirname + '/index.html',
+        function(err, data) {
+            if (err) {
+                res.writeHead(500, {
+                    "Content-Type": "text/plain"
+                });
+                return res.end('Error loading index.html');
+            }
+
+            res.writeHead(200, {
+                "Content-Type": "text/html"
+            });
+            res.end(data);
+        });
+}
