e06ffb089e8af2e8968324ad3a8d4ebd57d58919
raveclassic-home <raveclassic@gmail.com>
2012-06-01 01:41:25 +0400

    vk api

--- server.js
+++ server.js
@@ -1,52 +1,43 @@
-var app = require('http').createServer(handler);
+﻿var http = require('http');
+var app = http.createServer(handler);
 var io = require('socket.io').listen(app);
 var fs = require('fs');
-var db = require('./database.js');
 
-var test = require('./test.js');
+var vk = require('./vk.js');
+vk.init(http);
 
-var port = process.env.PORT || 8080;
-
-app.listen(port, function() {
-    console.log("Listening on port: ", port);
+app.listen(8080, function() {
+	console.log("listening to 8080");
 });
+
 var users = new Array();
-db.open(
-    function() {
-        console.log("Connection established");
-        console.log("Waiting for requests..");
-        io.sockets.on('connection',
-            function(socket) {
-                console.log("new client!");
-                socket.emit('logon', {
-                    currentOnline: users.length
-                });
-                socket.on('my other event',
-                    function(data) {
-                        console.log(data);
-                    });
-            }
-        );
-    },
-    function(err) {
-        console.dir(err);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {	
+    if (err) {
+      res.writeHead(500, {"Content-Type": "text/plain; charset=utf-8;"});
+      return res.end('Error loading index.html');
     }
-);
-
-
-function handler(req, res) {
-    fs.readFile(__dirname + '/index.html',
-        function(err, data) {
-            if (err) {
-                res.writeHead(500, {
-                    "Content-Type": "text/plain"
-                });
-                return res.end('Error loading index.html');
-            }
-
-            res.writeHead(200, {
-                "Content-Type": "text/html"
-            });
-            res.end(data);
-        });
+    res.writeHead(200, {"Content-Type": "text/html; charset=utf-8;"});
+	res.end(data);
+  });
 }
+ 
+io.sockets.on('connection', function (socket) {
+	socket.emit('message', { response: 'logged!' });
+	socket.on('message', function(request) {
+		console.log(request);
+		switch (request.api) {
+			case 'vk':
+				if (vk[request.methodName]) {
+					vk.getProfile(320520, function(response) {
+						socket.emit('message', { response: response });
+					});
+				} else {
+					socket.emit('message', { error: 'unknown method' });
+				}					
+			break;
+		}
+	});
+});
---
+++ vk.js
@@ -0,0 +1,60 @@
+﻿var apiKey = 2977876;
+var apiSecret = '8LofcpKjh4K87NPyhTOE';
+var host = 'api.vk.com';
+var path = '/api.php';
+var md5 = function(data) {
+	return require('crypto').createHash('md5').update(data).digest('hex');
+}
+var httpOptions = {
+	host: host,
+	port: 80,
+	path: path
+};
+
+var queryParams = [
+	"api_id="+apiKey,
+	"format=json",
+	"v=3.0"
+];
+
+var http = null;
+
+function init(app_http) {
+	http = app_http;
+}
+
+function generateQuery(methodName, methodParams) {
+	var newParams = queryParams.slice(0).concat(methodParams); 
+	newParams.push("method="+methodName);
+	newParams.sort();
+	return newParams.join("&") + "&sig="+md5(newParams.join('') + apiSecret); 
+};
+
+function getProfile(userId, callback) {
+	httpOptions.path = path + "?" + generateQuery("getProfiles", ["uids="+userId]);
+	console.dir(httpOptions);
+	var self = this;
+	var result = "";
+	http.get(httpOptions, function(res) {
+		//console.log(JSON.stringify(res.headers));
+		res.setEncoding('utf8');
+		var result = "";  
+		
+		res.on('data', function(chunk) {
+			//console.log(chunk);
+			result += chunk;
+		});
+		res.on('end', function() {
+			if (callback)
+				callback(JSON.parse(result));
+		});
+	}).on('error', function(e) {
+		console.log('got error: ');
+		console.dir(e);
+	});
+}
+
+//exports
+exports.init = init;
+exports.getProfile = getProfile;
+exports.generateQuery = generateQuery;
